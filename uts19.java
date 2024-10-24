4bff99c5957e0af345c67955fa5c15c60a673f91
Nadyaaurora <nadyaaurora575@gmail.com>
2024-10-24 10:29:16 +0700

    UTS DASPRO

---
+++ uts19.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class uts19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int baris = 5;
+        int kolom = 3;
+        
+        for (int i=0; i< baris; i++){ 
+            int n = i + 1;
+            for (int j = 0; j < kolom; j++) {
+            System.out.print(n + "\t"); ;
+            n += baris;
+            }
+        System.out.println();
+        }     
+    } 
+}
