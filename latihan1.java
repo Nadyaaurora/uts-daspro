804ee61905c49bbd050edcf9649d28d6fcfeac80
Nadyaaurora <nadyaaurora575@gmail.com>
2024-12-30 13:07:34 +0700

    uts daspro

---
+++ latihan1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class latihan1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p, q;
+        double totalBunga;
+
+        if (1 <= p){
+            totalBunga = (p*p + q*q + 1){
+            System.out.println(totalBunga + "bisa dibagikan");
+            }
+
+        }
+    }    
+}
